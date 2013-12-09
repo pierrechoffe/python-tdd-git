97ee303a2e9279a62b9453d5bcfdf563beeb0ca2
pierre choffe <pc@orchestredeschampselysees.com>
2013-12-09 23:15:35 +0100

    corrected error in template
    forgotten name = « item_text » in input

--- pythontdd/superlists/lists/tests.py
+++ pythontdd/superlists/lists/tests.py
@@ -1,11 +1,10 @@
 from django.core.urlresolvers import resolve
-from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.test import TestCase
 
+from lists.models import Item
 from lists.views import home_page
-
-class HomePageTest(TestCase):
 
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
